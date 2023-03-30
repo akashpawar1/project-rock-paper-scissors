ef8a3fed08a9997cbb3f155eeb6e8a1b78f2a0ba
akash <105465488+akashpawar1@users.noreply.github.com>
2023-03-30 09:46:18 +0530

    created game function that runs 5 time this game

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-function getComputerChoice(){
-    let choice = Math.floor(Math.random()*3);
-    if (choice===0) return 'rock';
-    if ( choice === 1 ) return 'paper';
-    if ( choice === 2 ) return 'scissor'
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice === 0) return "rock";
+  if (choice === 1) return "paper";
+  if (choice === 2) return "scissor";
 }
 
-let playerSelection = prompt('rock,paper , scissor').toLowerCase();
-
-function playRound(playerSelection, computerSelection = getComputerChoice()){
-    if (playerSelection === computerSelection) return `Game tie! both have ${computerSelection}`
-    if (playerSelection === 'rock' && computerSelection === 'paper') return 'you lose! paper beats rock';
-    if (playerSelection === 'rock' && computerSelection === 'scissor') return 'you win! rock beats scissor';
-    if (playerSelection === 'paper' && computerSelection === 'scissor') return 'you lose! scissor beats paper';
-    if (playerSelection === 'paper' && computerSelection === 'rock') return 'you win! paper beats rock';
-    if (playerSelection === 'scissor' && computerSelection === 'rock') return 'you lose! rock beats scissor';
-    if (playerSelection === 'scissor' && computerSelection === 'paper') return 'you win! scissor beats rock';
-    else return `${playerSelection} is not valid`;
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection)
+    return `Game tie! both have ${computerSelection}`;
+  if (playerSelection === "rock" && computerSelection === "paper")
+    return "you lose! paper beats rock";
+  if (playerSelection === "rock" && computerSelection === "scissor")
+    return "you win! rock beats scissor";
+  if (playerSelection === "paper" && computerSelection === "scissor")
+    return "you lose! scissor beats paper";
+  if (playerSelection === "paper" && computerSelection === "rock")
+    return "you win! paper beats rock";
+  if (playerSelection === "scissor" && computerSelection === "rock")
+    return "you lose! rock beats scissor";
+  if (playerSelection === "scissor" && computerSelection === "paper")
+    return "you win! scissor beats rock";
+  else return `${playerSelection} is not valid`;
 }
 
-console.log(playRound(playerSelection));
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("rock,paper , scissor").toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection,computerSelection));
+  }
+}
+
+game();
